864aac8a929a1308cacfeb8be647f58ebfc82b4a
Changyu_Liu <liuchangyu1111@gmail.com>
2019-11-03 22:43:46 +0800

    Create quick_sort.py

---
+++ 04-quick-sort-algorithm/quick_sort.py
@@ -0,0 +1,28 @@
+# Copyright 2019 Lorna Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
+def quick_sort(arr):
+  if len(arr) < 2:
+    return arr
+  else:
+    pivot = arr[0]
+    less = [i for i in arr[1:] if i <= pivot]
+
+    greater = [i for i in arr[1:] if i > pivot]
+
+    return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+print(quick_sort([10, 5, 2, 3]))
